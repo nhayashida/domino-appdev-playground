b540f0574929aabc70237a1291fef70c6401448b
Norimasa Hayashida <nhayashida@gmail.com>
2018-11-28 03:11:28 +0900

    update libs

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,4 +1,3 @@
-import UglifyEsPlugin from 'uglify-es-webpack-plugin';
 import { DefinePlugin } from 'webpack';
 import merge from 'webpack-merge';
 import common from './webpack.common';
@@ -10,9 +9,6 @@ const prod = merge(common, {
       NODE_ENV: JSON.stringify('production'),
     }),
   ],
-  optimization: {
-    minimizer: [new UglifyEsPlugin()],
-  },
 });
 
 export default prod;
