11e05427f284264cdb283b19da38faf28697435d
Norimasa Hayashida <nhayashida@gmail.com>
2018-11-28 03:17:36 +0900

    mobile

--- src/client/components/dql/Html.tsx
+++ src/client/components/dql/Html.tsx
@@ -8,6 +8,12 @@ export default class Html extends PureComponent<Props> {
   render() {
     return (
       <html>
+        <header>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          />
+        </header>
         <body>
           <div id="app">{this.props.children}</div>
           <script src="/vendor.bundle.js" />
